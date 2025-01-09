b93c1d86ad073f59c2a3d7b335a0f17bf15a9e84
Arenas07 <189859279+Arenas07@users.noreply.github.com>
2025-01-09 09:18:32 -0500

    -Creacion de nuevas opciones para el menu -Modificación de archivo JSON -Modificación de inventario

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from design.products import design, tableProducts
+from design.products import design, tableProducts, tableProductsByCategory
+from logic.products import updateInventoryByCode
 
 match design():
     case 1:
         tableProducts()
+    case 2:
+        tableProductsByCategory(input("Ingrese la categoria. Ejemplo ('panes', 'pastel', 'postres'): ").lower())
+    case 3:
+        updateInventoryByCode(input("Ingrese el codigo del producto: "))
+    case 0:
+        print("Chao")
     case _:
         print("Esa opción no existe")
