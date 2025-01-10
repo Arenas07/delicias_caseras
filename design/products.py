c33f4a961ec1acec94e888f6fd6aa5e24ff350c3
Arenas07 <189859279+Arenas07@users.noreply.github.com>
2025-01-10 17:26:32 -0500

    -Se implementó nuevo menu diferenciando entre pedidos y actualizazcion de stock+informacion, nueva carpeta de diseño general simplificando el main,+

--- design/products.py
+++ design/products.py
@@ -1,20 +1,22 @@
-from logic.products import findAll
+from logic.products import findAll, saveAll
 from tabulate import tabulate
 
 def obtener_opcion():
     while True:
         try:
             print("""
+        *****************************************************
             Menu de productos
                 1. Ver productos
                 2. Ver productos por categoria
-                3. Buscar producto por código
-                4. Buscar producto por nombre
+                3. Ver producto por código
+                4. Ver producto por nombre
                 5. Actualizar el inventario de un producto
                 6. Agregar un nuevo producto al stock
-                0. Salir 
+                0. Salir
+        *****************************************************           
             """)
-            opcion = int(input())
+            opcion = int(input("--> "))
             if 0 <= opcion <= 6:  # Asegura que la opción esté dentro de un rango válido
                 return opcion
             else:
@@ -31,7 +33,7 @@ def tableProducts():
         diccionario.pop("precio_proveedor")
         dataModify.append(diccionario)
     print(tabulate(dataModify, headers="keys", tablefmt="grid", numalign="center", showindex="always"))
-
+    input("--> Presione enter para continuar")
 def tableProductsByCategory(Category):
     data = findAll()
     dataModify = []
@@ -42,4 +44,53 @@ def tableProductsByCategory(Category):
             diccionario.pop("precio_proveedor")
             dataModify.append(diccionario)
     print(tabulate(dataModify, headers="keys", tablefmt="grid", numalign="center", showindex="always"))
+    input("--> Presione enter para continuar")
 
+def tableProductsByCode(Code):
+    data = findAll()
+    dataModify = []
+    for diccionario in data:
+        if(diccionario.get("codigo_producto") == Code):
+            diccionario.pop("descripcion")
+            diccionario.pop("proveedor")
+            diccionario.pop("precio_proveedor")
+            dataModify.append(diccionario)
+    print(tabulate(dataModify, headers="keys", tablefmt="grid", numalign="center", showindex="always"))
+    input("--> Presione enter para continuar")  
+
+def tableProductsByName(Name):
+    data = findAll()
+    dataModify = []
+    for diccionario in data:
+        if(diccionario.get("nombre") == Name):
+            diccionario.pop("descripcion")
+            diccionario.pop("proveedor")
+            diccionario.pop("precio_proveedor")
+            dataModify.append(diccionario)
+    print(tabulate(dataModify, headers="keys", tablefmt="grid", numalign="center", showindex="always"))
+    input("--> Presione enter para continuar")        
+def newProduct():
+    data = findAll()
+    codigo_producto = input("Ingrese el código del producto: ")
+    findProducts = list(filter(lambda product: product.get("codigo_producto") == codigo_producto, data))
+    if(not len(findProducts)):
+        newProduct = {
+            "codigo_producto": codigo_producto,
+            "nombre": input("Ingrese el nombre del producto: "),
+            "categoria": input("Ingrese la categoria del producto ('panes', 'pastel', 'postre'): ").lower(),
+            "descripcion": input("Ingrese la descripción del producto: "),
+            "proveedor": input("Ingrese el proveedor de dicho producto: "),
+            "cantidad_en_stock": int(input("Ingrese la cantidad en stock del producto: ")),
+            "precio_venta": float(input("Ingrese el precio de venta del producto: ")),
+            "precio_proveedor": float(input("Ingrese el precio del proveedor: "))
+     }
+        data.append(newProduct)
+        saveAll(data)
+        return "Producto agregado exitosamente"
+        
+    else:
+        print("""
+        *********************
+            ERROR-ERROR 
+        El producto ya existe
+        *********************""")
--- main.py
+++ main.py
@@ -1,28 +1,5 @@
-from design.products import tableProducts, tableProductsByCategory, obtener_opcion
-from logic.products import updateInventoryByCode, newProduct
+
+from design.general import *
+menu()
 
 
-
-
-
-
-
-
-
-
-while True:
-    option = obtener_opcion()
-    match option:
-        case 1:
-            tableProducts()
-        case 2:
-            tableProductsByCategory(input("Ingrese la categoria. Ejemplo ('panes', 'pastel', 'postre'): ").capitalize())
-        case 3:
-            updateInventoryByCode(input("Ingrese el codigo del producto: "))
-        case 4:
-            newProduct() 
-        case 0:
-            print("Chao")
-            break
-
-#NECESITO CORREGIR LOS CASES    
